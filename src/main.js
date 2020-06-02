3fdec24ca98fae8c4816d705bace38e8abe95fc7
REN·WANG <842837175@qq.com>
2020-06-02 09:54:37 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
+import VueFullPage from 'vue-fullpage.js'
+import './comm.scss'
 
+Vue.use(VueFullPage)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-page">
+    <full-page ref="fullpage" :options="options">
+      <div class="section">
+        1 section ...
+      </div>
+      <div class="section">
+        2 section ...
+      </div>
+      <div class="section">
+        3 section ...
+      </div>
+    </full-page>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {},
+    data() {
+      return {
+        options: {
+          licenseKey: null,
+          sectionsColor: ['#41b883', '#ff5f45', '#0798ec']
+        }
+      }
+    },
+    methods: {
+      // Called when your components are ready. That is up to you to decide when.
+      componentsReady() {
+        this.$refs.fullpage.init()
+      }
+    },
+    mounted() {
+      this.componentsReady()
+    }
   }
-}
 </script>
